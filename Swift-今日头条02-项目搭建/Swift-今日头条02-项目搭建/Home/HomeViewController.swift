ddee939d4a7aed915c8eab3e197fccb6967c6dd9
Evan Yang <evanyang966@163.com>
2020-01-30 22:57:03 +0800

    Initial Commit

---
+++ Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Swift-今日头条02-项目搭建
+//
+//  Created by Evan Yang on 2020/1/29.
+//  Copyright © 2020 Evan Yang. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
