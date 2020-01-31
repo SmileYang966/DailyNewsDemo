a1b0875b34661b9d9c217a3167551c1605ce48e2
Evan Yang <evanyang966@163.com>
2020-01-31 13:15:17 +0800

    SC02-Changed the initial setting screen

--- Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Mine/MySettingViewController.swift
+++ Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Mine/MySettingViewController.swift
@@ -12,7 +12,7 @@ class MySettingViewController: UIViewController,UITableViewDelegate,UITableViewD
     
     //1.Swift方式定义一个tableView
     lazy var mySettingTableView : UITableView = {
-        var tableView : UITableView = UITableView(frame: self.view.bounds, style: UITableView.Style.grouped)
+        var tableView : UITableView = UITableView(frame: self.view.bounds, style: UITableView.Style.plain)
         
         tableView.delegate = self
         tableView.dataSource = self;
@@ -21,10 +21,25 @@ class MySettingViewController: UIViewController,UITableViewDelegate,UITableViewD
         return UITableView()
     }()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.orange;
+        
+        self.mySettingTableView.backgroundColor = UIColor.lightGray
+    }
+}
+
+extension MySettingViewController{
     
     //UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -39,14 +54,9 @@ class MySettingViewController: UIViewController,UITableViewDelegate,UITableViewD
     }
     
     //UITableViewDelegate
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.orange;
-        
-        self.mySettingTableView.backgroundColor = UIColor.lightGray
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 20.0))
+        headerView.backgroundColor = UIColor.init(red: 247/255.0, green: 248/255.0, blue: 249/255.0, alpha: 1.0)
+        return headerView
     }
 }
---
+++ Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Others/const.swift
@@ -0,0 +1,17 @@
+//
+//  const.swift
+//  Swift-今日头条02-项目搭建
+//
+//  Created by Evan Yang on 2020/1/31.
+//  Copyright © 2020 Evan Yang. All rights reserved.
+//
+
+import UIKit
+
+//定义一些常量
+let screenWidth = UIScreen.main.bounds.size.width
+let screenHeight = UIScreen.main.bounds.size.height
+
+
+
+
