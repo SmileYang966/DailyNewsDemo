c9144d43d665aa7bb152c6e15e12ed7f87735961
Evan Yang <evanyang966@163.com>
2020-01-31 09:33:22 +0800

    1. Swift创建的第一个TableView

--- Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Mine/MySettingViewController.swift
+++ Swift-今日头条02-项目搭建/Swift-今日头条02-项目搭建/Mine/MySettingViewController.swift
@@ -8,24 +8,45 @@
 
 import UIKit
 
-class MySettingViewController: UIViewController {
-
+class MySettingViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    //1.Swift方式定义一个tableView
+    lazy var mySettingTableView : UITableView = {
+        var tableView : UITableView = UITableView(frame: self.view.bounds, style: UITableView.Style.grouped)
+        
+        tableView.delegate = self
+        tableView.dataSource = self;
+        
+        self.view.addSubview(tableView)
+        return UITableView()
+    }()
+    
+    
+    //UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID : String = "CELLID";
+        var tableViewCell : UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if tableViewCell == nil {
+            tableViewCell = UITableViewCell(style: .value1, reuseIdentifier: cellID);
+        }
+        tableViewCell!.textLabel!.text = "12345"
+        
+        return tableViewCell!
+    }
+    
+    //UITableViewDelegate
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.orange;
+        
+        self.mySettingTableView.backgroundColor = UIColor.lightGray
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
